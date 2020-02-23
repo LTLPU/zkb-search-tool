dde8e9ed263ee73a0df68bb7c07678e8826b3291
LTLPU <LTLBAZAAR>
2020-02-23 17:17:40 +0900

    dark追加(暫定)

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -33,6 +33,32 @@ export default new Vuetify({
         region: colors.teal.darken4,
         ship: colors.brown.darken4,
         group: colors.brown.darken4
+      },
+      dark: {
+        primary: colors.blue.lighten2,
+        secondary: colors.red.darken4,
+        accent: colors.indigo.base,
+        kills: colors.green.lighten4,
+        losses: colors.red.lighten4,
+        solo: colors.green.lighten4,
+        highsec: colors.blue.lighten4,
+        lowsec: colors.orange.lighten4,
+        nullsec: colors.red.lighten4,
+        wspace: colors.purple.lighten4,
+        abyssal: colors.grey.lighten4,
+        finalblow: colors.pink.lighten4,
+        iskvalue: colors.cyan.lighten4,
+        isk5b: colors.amber.lighten4,
+        isk10b: colors.orange.lighten4,
+        isk20b: colors.red.lighten4,
+        character: colors.blueGrey.lighten4,
+        corporation: colors.blueGrey.lighten4,
+        alliance: colors.blueGrey.lighten4,
+        system: colors.teal.lighten4,
+        constellation: colors.teal.lighten4,
+        region: colors.teal.lighten4,
+        ship: colors.brown.lighten4,
+        group: colors.brown.lighten4
       }
     }
   }
